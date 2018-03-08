6a7578e49cd43e5470727acc85a97f0d3096f9a4
David Haynes <DavidHaynesVictoria@gmail.com>
2018-03-07 16:46:26 -0800

    have initial html and css elements designed. main paige, restaurant container, menu, checkout and login all working. next will be to implement the routes for the buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,21 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+   $('.loginreg').hide();
+   $('.shopping-cart').hide();
+
+$(document).ready(function() {
+
+  $("#loginregbtn").click(function() {
+    if($('.shopping-cart').is(':visible')){
+       $('.shopping-cart').hide();
     }
-  });;
-});
+    $(".loginreg").fadeToggle();
+  })
+
+  $("#cart").on("click", function() {
+    if($('.loginreg').is(':visible')){
+       $('.loginreg').hide();
+    }
+    $(".shopping-cart").fadeToggle();
+  });
+
+
+})
