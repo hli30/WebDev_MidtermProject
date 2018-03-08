4402d525142d26a7685d825454042ffb8392123c
David Haynes <DavidHaynesVictoria@gmail.com>
2018-03-07 20:49:51 -0800

    started adding render and load functions in app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,9 @@
    $('.loginreg').hide();
    $('.shopping-cart').hide();
+   $('#menu').hide();
 $(document).ready(function() {
-
+  $(".button-collapse").sideNav();
+  
   $("#loginregbtn").click(function() {
     if($('.shopping-cart').is(':visible')){
        $('.shopping-cart').hide();
@@ -14,17 +16,44 @@ $(document).ready(function() {
        $('.loginreg').hide();
     }
     $(".shopping-cart").fadeToggle();
-
-
-
-
   });
 
+  $(".showRestaurantMenu").on("click", function() {
+       $('#restList.container').hide();
+       $("#menu").show();
+    })
+
+    $("#menuToRest").on("click", function() {
+       $("#menu").hide();
+       $('#restList.container').show();
+      })
+
+      const loadMenu = function() {
+        $.get("/restaurant/food", renderMenu);
+      };
+
+      const loadRestaurants= function() {
+        $.get("/restaurants", renderRestaurants);
+      };
+
+      const renderMenu= function(data) {
+        var source= $("#menuTemplate").html()
+
+        var template = Handlebars.compile(source);
+        var html = template(data);
+        $("#body").html(html);
+      }
+
+      const renderRestaurants= function(data) {
+        var source= $("#restTemplate").html()
+
+        var template = Handlebars.compile(source);
+        var html = template(data);
+        $("#body").html(html);
+      }
 
 
-
-
-
+      loadRestaurants()
 
 
 
