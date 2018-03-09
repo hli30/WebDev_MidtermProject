864ed1a6ce4241a6b00cfcb1211fe48dd43c745c
David <daivdhayensvictoria@gmail.com>
2018-03-09 23:18:48 +0000

    now have dynamic menus functioning wiht handlebars

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,68 +1,78 @@
-   $('.loginreg').hide();
-   $('.shopping-cart').hide();
-   $('#menu').hide();
+$('.loginreg').hide();
+$('.shopping-cart').hide();
+$('#menu').hide();
+$(function() {
+// Makes sure that the cart and login arent displayed at the same time
+  $("#loginregbtn").click(function() {
+    if($('.shopping-cart').is(':visible')){
+      $('.shopping-cart').hide();
+    }
+    $(".loginreg").fadeToggle();
+  });
 
-   $(function() {
-  // Makes sure that the cart and login arent displayed at the same time
-     $("#loginregbtn").click(function() {
-       if($('.shopping-cart').is(':visible')){
-         $('.shopping-cart').hide();
-       }
-       $(".loginreg").fadeToggle();
-     });
+  $("#cart").on("click", function() {
+    if($('.loginreg').is(':visible')){
+      $('.loginreg').hide();
+    }
+    $(".shopping-cart").fadeToggle();
+  });
 
-     $("#cart").on("click", function() {
-       if($('.loginreg').is(':visible')){
-         $('.loginreg').hide();
-       }
-       $(".shopping-cart").fadeToggle();
-     });
+  //shows or hides the menu/restaurant list as needed
+  $(".showRestaurantMenu").on("click", function() {
+    $('#restList.container').hide();
+    $("#menu").show();
+  });
 
-     //shows or hides the menu/restaurant list as needed
-     $(".showRestaurantMenu").on("click", function() {
-       $('#restList.container').hide();
-       $("#menu").show();
-     });
-
-     $("#menuToRest").on("click", function() {
-       $("#menu").hide();
-       $('#restList.container').show();
-     });
-
-
-     $("#restlist").on("click", '[data-restaurant-id]', function(event) {
-       event.preventDefault();
-       const restaurantId = $(this).data('restaurantId');
-       console.log('it clicks', restaurantId);
-       $('#restaurantByID').fadeToggle();
-     });
-
-    //renders the menu data with handlebars
-     const renderMenu = function(data) {
-       var source = $("#menuTemplate").html();
-
-       var template = Handlebars.compile(source);
-       var html = template(data);
-       $("#body").html(html);
-     };
-    //renders the restaurant data with handlebars
-     const renderRestaurants = function(restaurants) {
-       var source = $("#restaurantTemplate").html();
-
-       var template = Handlebars.compile(source);
-       var templateHtml = template(restaurants);
-       $("#restlist").html(templateHtml);
-     };
+  $("#menu").on("click", '[data-return-toRest]', function() {
+    // $("#stupidTemp").replaceWith('{{#menu}} {{> menuTemp}} {{/menu}}');
+    console.log('cleared the dom?');
+    $('#restList.container').show();
+    $("#menu").hide();
+  });
 
 
-         //loads the menu data
-     const loadMenu = function() {
-       $.get("/restaurant/food", renderMenu);
-     };
-   //loads the restaurant data
-     const loadRestaurants = function() {
-       $.get("/restaurant", renderRestaurants);
-     };
-      //calls the load restaurant lists
-     loadRestaurants();
-   });
+  //renders the menu data with handlebars
+  function makeTemplateFnFromId(id){
+    var source = $(id).html();
+    var templateFn = Handlebars.compile(source);
+    return templateFn;
+  }
+  function makePartialWithId(name){
+    const source = $('#' + name).html();
+    Handlebars.registerPartial(name, source);
+  }
+
+  makePartialWithId('menuTemp');
+  const menuTemplate = makeTemplateFnFromId('#menuTemplate');
+
+  const renderMenu = function(menuData) {
+    var templateHtml = menuTemplate(menuData);
+    console.log('this is templatehtml for menu', templateHtml);
+    $("#menu").html(templateHtml);
+  };
+
+  //renders menu based on the restaurant click
+  $("#restlist").on("click", '[data-restaurant-id]', function(event) {
+    const restaurantId = $(this).data('restaurantId');
+    $('#restList.container').hide();
+    $("#menu").show();
+    console.log(`/restaurant/${restaurantId}`);
+    $.get(`/restaurant/${restaurantId}`, renderMenu);
+  });
+
+  //renders the restaurant data with handlebars
+  const renderRestaurants = function(restaurants) {
+    var source = $("#restaurantTemplate").html();
+    var template = Handlebars.compile(source);
+    var templateHtml = template(restaurants);
+    $("#restlist").html(templateHtml);
+  };
+
+  //loads the restaurant data
+  const loadRestaurants = function() {
+    $.get("/restaurant", renderRestaurants);
+  };
+
+  //calls the load restaurant lists
+  loadRestaurants();
+});
